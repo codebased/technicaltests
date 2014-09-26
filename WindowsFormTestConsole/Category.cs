7be1cba222216496bb19f33c6760a2fa79a91a5b
Amit Malhotra <codebased@hotmail.com>
2014-09-27 09:41:08 +1000

    Windows Form Data Grid View with EF

---
+++ WindowsFormTestConsole/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormTestConsole
+{
+    public class Category
+    {
+        private readonly ObservableListSource<Product> _products =
+                new ObservableListSource<Product>();
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public virtual ObservableListSource<Product> Products { get { return _products; } }
+    } 
+}
